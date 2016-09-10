d2827c53d37da1490b126fb2dff4119f29d85fff
Craig Kochis <cjkochis@gmail.com>
2016-09-10 13:43:21 -0400

    add eslint global config

--- setup.sh
+++ setup.sh
@@ -3,5 +3,9 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/gvimrc ~/.gvimrc
+ln -s ~/.vim/eslintrc.json ~/.eslintrc.json
 
 vim +PluginInstall +qall
+
+echo "Make sure the npm package 'eslint' is installed globally:"
+echo "  npm install eslint -g"
